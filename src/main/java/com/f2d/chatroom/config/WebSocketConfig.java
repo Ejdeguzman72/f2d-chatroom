ae92fe75b530639f64e5d0b9e53f6a0a20f98184
Ejdeguzman72 <ejdeguzman72@gmail.com>
2025-02-19 23:57:25 -0500

    more changes

--- src/main/java/com/f2d/chatroom/config/WebSocketConfig.java
+++ src/main/java/com/f2d/chatroom/config/WebSocketConfig.java
@@ -1,55 +1,24 @@
 package com.f2d.chatroom.config;
 
-import com.f2d.chatroom.domain.ChatGroup;
-import com.f2d.chatroom.domain.ChatGroupListResponse;
-import com.f2d.chatroom.service.F2DChatGroupService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
-    private static final Logger logger = LoggerFactory.getLogger(WebSocketConfig.class);
-    private final DynamicWebSocketHandler dynamicWebSocketHandler;
-    @Autowired
-    private F2DChatGroupService chatGroupService;
-
-    // Constructor injection of DynamicWebSocketHandler
-    public WebSocketConfig(DynamicWebSocketHandler dynamicWebSocketHandler) {
-        this.dynamicWebSocketHandler = dynamicWebSocketHandler;
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/f2d-chat")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        // Registering the default channel handler
-        registry.addHandler(new DynamicWebSocketHandler.F2DWebSocketHandler("default"), "/ws/default")
-                .setAllowedOrigins("*");
-
-        // Retrieve chat groups and register dynamic handlers
-        try {
-            ChatGroupListResponse chatGroupListResponse = chatGroupService.retrieveAllChatGroupInfo();
-            if (chatGroupListResponse != null && chatGroupListResponse.getList() != null) {
-                for (ChatGroup chatGroup : chatGroupListResponse.getList()) {
-                    registry.addHandler(new DynamicWebSocketHandler.F2DWebSocketHandler(chatGroup.getGroupName()),
-                                    "/ws/" + chatGroup.getGroupName())
-                            .setAllowedOrigins("*");
-                }
-            } else {
-                logger.error("No chat groups found or list is null");
-            }
-        } catch (Exception e) {
-            logger.error("Failed to retrieve chat groups: {}", e.getMessage());
-        }
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
     }
-
-    // Dynamically register all available channels
-//        dynamicWebSocketHandler.getChannels().forEach(channel -> {
-//            registry.addHandler(new DynamicWebSocketHandler.F2DWebSocketHandler(channel), "/ws/" + channel)
-//                    .setAllowedOrigins("*");
-//        });
 }
--- src/main/java/com/f2d/chatroom/controller/ChatController.java
+++ src/main/java/com/f2d/chatroom/controller/ChatController.java
@@ -1,16 +1,28 @@
 package com.f2d.chatroom.controller;
 
 import com.f2d.chatroom.domain.ChatMessage;
+import com.f2d.chatroom.domain.ChatMessageAddUpdateRequest;
+import com.f2d.chatroom.domain.ChatMessageAddUpdateResponse;
+import com.f2d.chatroom.service.F2DChatMessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class ChatController {
 
+    @Autowired
+    private F2DChatMessageService chatMessageService;
+
     @MessageMapping("/sendMessage")
-    @SendTo("/topic/messages")
-    public ChatMessage send(ChatMessage chatMessage) {
-        return chatMessage;
+    @SendTo("/topic/public")
+    public ChatMessageAddUpdateResponse send(@Payload ChatMessageAddUpdateRequest chatMessage) {
+        ChatMessageAddUpdateResponse response = chatMessageService.createChatMessage(chatMessage);
+
+        return response;
     }
 }
