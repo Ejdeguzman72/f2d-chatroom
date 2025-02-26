34475d1ab1522cdbed3c5f16971e55657f6f37a8
Ejdeguzman72 <ejdeguzman72@gmail.com>
2025-02-26 01:10:26 -0500

    more changes

--- src/main/java/com/f2d/chatroom/config/ChatWebSocketHandler.java
+++ src/main/java/com/f2d/chatroom/config/ChatWebSocketHandler.java
@@ -1,5 +1,6 @@
 package com.f2d.chatroom.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -8,27 +9,32 @@ import java.util.concurrent.CopyOnWriteArraySet;
 
 public class ChatWebSocketHandler extends TextWebSocketHandler {
     private static final Set<WebSocketSession> sessions = new CopyOnWriteArraySet<>();
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+    public void afterConnectionEstablished(WebSocketSession session) {
         sessions.add(session);
         System.out.println("New user connected: " + session.getId());
     }
 
     @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) {
         System.out.println("Received message: " + message.getPayload());
 
-        // Broadcast message to all connected clients
+        // Broadcast the message to all connected clients
         for (WebSocketSession s : sessions) {
             if (s.isOpen()) {
-                s.sendMessage(message);
+                try {
+                    s.sendMessage(new TextMessage(message.getPayload()));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
     @Override
-    public void afterConnectionClosed(WebSocketSession session, org.springframework.web.socket.CloseStatus status) throws Exception {
+    public void afterConnectionClosed(WebSocketSession session, org.springframework.web.socket.CloseStatus status) {
         sessions.remove(session);
         System.out.println("User disconnected: " + session.getId());
     }
