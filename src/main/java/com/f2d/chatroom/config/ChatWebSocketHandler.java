4ef8d4f8aed180018cc98570f25a531a3b748933
Ejdeguzman72 <ejdeguzman72@gmail.com>
2025-02-26 23:51:12 -0500

    more changes

--- src/main/java/com/f2d/chatroom/config/ChatWebSocketHandler.java
+++ src/main/java/com/f2d/chatroom/config/ChatWebSocketHandler.java
@@ -1,15 +1,26 @@
 package com.f2d.chatroom.config;
 
+import com.f2d.chatroom.domain.ChatMessage;
+import com.f2d.chatroom.repository.F2DChatMessageRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 public class ChatWebSocketHandler extends TextWebSocketHandler {
     private static final Set<WebSocketSession> sessions = new CopyOnWriteArraySet<>();
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    private F2DChatMessageRepository chatMessageRepository;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ChatWebSocketHandler.class);
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
@@ -18,8 +29,14 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
     }
 
     @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) {
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws JsonProcessingException {
         System.out.println("Received message: " + message.getPayload());
+
+        ChatMessage chatMessage = objectMapper.readValue(message.getPayload(), ChatMessage.class);
+        chatMessage.setSentDatetime(LocalDateTime.now());
+
+        chatMessageRepository.save(chatMessage);
+        LOGGER.info("Saving Message to database: " + chatMessage.toString());
 
         // Broadcast the message to all connected clients
         for (WebSocketSession s : sessions) {
